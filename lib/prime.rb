d3bf233bb89ac4826f74ca0bd10c80b2836ae5bf
Ryna Truong <rynatruong@outlook.com>
2020-06-23 16:44:12 +0000

    Automatically backed up by Learn

--- lib/prime.rb
+++ lib/prime.rb
@@ -1,3 +1,9 @@
-def prime(int)
-  if int
+def prime?(int)
+  if int < 0 || int == 0 || int == 1
+      false
+    else
+      (2..int-1).to_a.all? do |num|
+        int % num != 0
+    end
+  end
 end
